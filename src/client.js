0a2348a6689d6c36b079527315d014fd86a0e79c
Amanda Romero <mandicap@comcast.net>
2021-11-04 09:36:09 -0600

    Connects Sanity to React frontend

---
+++ src/client.js
@@ -0,0 +1,7 @@
+import SanityClient from "@sanity/client";
+
+export default SanityClient({
+  projectId: '57644kxy',
+  dataset: 'production',
+  useCdn: process.env.NODE_ENV === 'production' ? true : false
+});
