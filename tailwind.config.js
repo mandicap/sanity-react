6e7fed72291af56637cf518b7bb7199f0b2d16b0
Amanda Romero <mandicap@comcast.net>
2021-11-06 00:18:58 -0600

    Curates default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      red: colors.rose,
+      green: colors.emerald,
+      blue: colors.blueGray,
+    },
     extend: {},
   },
   variants: {
