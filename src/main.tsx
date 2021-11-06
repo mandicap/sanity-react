f85659f4a6e13637b3a3390feaba6c0b98d11d02
Amanda Romero <mandicap@comcast.net>
2021-11-05 23:57:31 -0600

    Nests blog routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import App from './App'
-import Post from './components/Post'
-import Posts from './components/Posts'
+import App from './App';
+import Post from './components/Post';
+import Posts from './components/Posts';
 
-import './index.css'
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
-          <Route path="blog" element={<Posts />} />
-          <Route path="blog/:slug" element={<Post />} />
+          {/* Blog Routes */}
+          <Route path="blog" element={<Posts />}>
+            <Route path=":slug" element={<Post />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
